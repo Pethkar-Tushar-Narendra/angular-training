06cb9414fff73f95c6188a5793c90453b9349e02
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-06-14 15:23:21 +0530

    interposlation and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: '[app-root]',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'second-day-trining';
+  title = 'second-day-training';
+  variableForInterpolation = 'variableForInterpolation value';
+  stylesForBinding = {
+    'font-size': '40px',
+    'font-weight': 'bold',
+    color: 'blue',
+  };
+  classForBinding = {
+    'text-green': true,
+  };
+  setStyles() {
+    let styles = {
+      'font-size': '40px',
+      'font-weight': 'bold',
+      color: 'red',
+    };
+    return styles;
+  }
 }
